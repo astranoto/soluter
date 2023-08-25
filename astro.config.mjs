0d512cbb1a53b18548211786e66717657ef50a30
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-25 19:07:19 +0200

    feat: publish on github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	integrations: [tailwind()],
+	site: 'https://astranoto.github.io',
+	base: '/soluter',
 });

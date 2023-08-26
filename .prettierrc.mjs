569716562305242261995f289531ab09ba4e9078
ilcors-dev <corsetti.luca28@gmail.com>
2023-08-26 19:07:30 +0200

    feat: prettier & tailwindcss configurations

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+export default {
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};

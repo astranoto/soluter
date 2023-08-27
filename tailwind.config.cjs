1469d9e1768011b295bd9b78d380fecedd863db9
ilcors-byte <ilcorsbyte@gmail.com>
2023-08-27 18:11:31 +0200

    fix:merge

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,21 @@ module.exports = {
     "./src/resources/**/*.json",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soluter-purple": {
+          500: "#7B3483",
+        },
+        "soluter-blue": {
+          500: "#1C5BA7",
+        },
+        "soluter-green": {
+          500: "#41A46C",
+        },
+        "soluter-orange": {
+          500: "#EE7838",
+        },
+      },
+    },
   },
 };

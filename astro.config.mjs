551599444bbccb1ea137b1f09cdb4ad829413a39
ilcors-dev <corsetti.luca28@gmail.com>
2023-09-07 18:08:47 +0200

    feat: seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
   site: "https://astranoto.github.io",
   base: "/soluter",
+  
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), compress(), robotsTxt()],
 });
